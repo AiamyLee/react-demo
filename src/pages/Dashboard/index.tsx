c9f254c6ad61b6e1bbe430a9b97c53fa5076be97
AiamyLee <lichunhui@moseeker.com>
2023-04-25 23:59:46 +0800

    save

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Dashboard extends Component {
   constructor(props: any) {
     super(props);
     this.state = {};
   }
-
+  //
   render() {
     return <div>11</div>;
   }
